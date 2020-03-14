11b7840d2fc1c4b25c70dc833b245d967d71bc2f
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2020-03-14 17:16:14 +0900

    Create Demo Project ,  gitignore

---
+++ YappProject/YappProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YappProject
+//
+//  Created by 조경진 on 2020/03/14.
+//  Copyright © 2020 조경진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
