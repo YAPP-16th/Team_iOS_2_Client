ea031e09a5206372f52a21eb90a0c9ae24599e2e
jmzzang <dream7739@naver.com>
2020-04-05 02:01:01 +0900

    앨범 수량설정 UI 완료

--- 90's/90's/Album/CreateAlbum/AlbumThemeController.swift
+++ 90's/90's/Album/CreateAlbum/AlbumThemeController.swift
@@ -4,7 +4,14 @@ import Foundation
 import UIKit
 
 class AlbumThemeController : UIViewController {
-    var albumIndex: Int?
+ //   var albumIndex: Int?
+ 
+    var albumName:String?
+    var startDate:String?
+    var expireDate:String?
+    var quantity:Int?
+    
+    
     
     @IBOutlet weak var themeTableView: UITableView!
     
@@ -17,8 +24,8 @@ class AlbumThemeController : UIViewController {
     }
     
     func setThemeTableView(){
-        themeTableView.delegate = self
-        themeTableView.dataSource = self
+//        themeTableView.delegate = self
+//        themeTableView.dataSource = self
     }
     
     
@@ -26,39 +33,39 @@ class AlbumThemeController : UIViewController {
 
 
 
-extension AlbumThemeController : UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        AlbumModel.albumList[albumIndex!].selectTheme = ThemeModel.themeList[indexPath.row]
-        let albumDetailVC = storyboard?.instantiateViewController(identifier: "albumDetailVC") as! AlbumDetailController
-        
-        albumDetailVC.albumIndex = self.albumIndex!
-        
-        //선택한 테마를 album에 넣어줌
-        AlbumModel.albumList[albumIndex!].selectTheme = ThemeModel.themeList[indexPath.row]
-        
-        navigationController?.pushViewController(albumDetailVC, animated: true)
-        
-    }
-}
+//extension AlbumThemeController : UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        AlbumModel.albumList[albumIndex!].selectTheme = ThemeModel.themeList[indexPath.row]
+//        let albumDetailVC = storyboard?.instantiateViewController(identifier: "albumDetailVC") as! AlbumDetailController
+//
+//        albumDetailVC.albumIndex = self.albumIndex!
+//
+//        //선택한 테마를 album에 넣어줌
+//        AlbumModel.albumList[albumIndex!].selectTheme = ThemeModel.themeList[indexPath.row]
+//
+//        navigationController?.pushViewController(albumDetailVC, animated: true)
+//
+//    }
+//}
 
 
-extension AlbumThemeController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ThemeModel.themeList.count
-    }
+//extension AlbumThemeController : UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return ThemeModel.themeList.count
+//    }
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "themeCell") as! ThemeCell
+//        let data = ThemeModel.themeList[indexPath.row]
+//        cell.themeImageView.image = UIImage(named: data.previewPhoto)
+//        cell.themeNameLabel.text = data.themeName
+//        return cell
+//
+//    }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "themeCell") as! ThemeCell
-        let data = ThemeModel.themeList[indexPath.row]
-        cell.themeImageView.image = UIImage(named: data.previewPhoto)
-        cell.themeNameLabel.text = data.themeName
-        return cell
-        
-    }
-    
-    
-}
+//}
