e84de9d0026d199b0b2bf9933b67b51c748a4559
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2020-04-05 18:30:12 +0900

    optionView 1차

--- 90's/90's/Print/OptionViewController.swift
+++ 90's/90's/Print/OptionViewController.swift
@@ -11,16 +11,114 @@ import UIKit
 class OptionViewController : UIViewController {
     
     
-    @IBOutlet weak var bottomViewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var optionView: UIView!
-    @IBOutlet weak var completeBtn: UIButton!
+    @IBOutlet weak var BottomViewConstraint: NSLayoutConstraint!
     
-    var isColorViewAppear = false
+    
+    @IBOutlet weak var FirstOptionConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var SecondOptionConstraint: NSLayoutConstraint!
+    @IBOutlet weak var OptionView: UIView!
+    @IBOutlet weak var CompleteBtn: UIButton!
+    @IBOutlet weak var FirstOptionView: UIView!
+    @IBOutlet weak var SecondOptionView: UIView!
+    
+    @IBOutlet weak var stackViewConnected: NSLayoutConstraint!
+    var isTotalOptionViewAppear = false
+    var isFirstOptionViewAppear = true
+    var isSecondOptionViewAppear = true
 
     
     
     override func viewDidLoad() {
+        self.view.backgroundColor = .white
         super.viewDidLoad()
+        
+        if !isTotalOptionViewAppear {
+            self.BottomViewConstraint.constant = self.view.frame.height
+            self.FirstOptionView.isHidden = true
+            self.SecondOptionView.isHidden = true
+            self.FirstOptionConstraint.constant = 0
+            self.SecondOptionConstraint.constant = 0
+            self.stackViewConnected.constant = 63.5
+        }
+        
+        
+    }
+    
+    
+    @IBAction func OptionClick(_ sender: Any) {
+        
+        isTotalOptionViewAppear = !isTotalOptionViewAppear
+        
+        if isTotalOptionViewAppear {
+            
+            self.BottomViewConstraint.constant = self.view.frame.height
+        } else {
+            self.view.backgroundColor = .darkGray
+            self.BottomViewConstraint.constant = 450
+        }
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [], animations: {self.view.layoutIfNeeded()})
+        
+    }
+    
+    @IBAction func CancelAction(_ sender: Any) {
+        self.view.backgroundColor = .white
+        self.BottomViewConstraint.constant = self.view.frame.height
+        
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [], animations: {self.view.layoutIfNeeded()})
+    }
+    
+    
+    @IBAction func CompleteBtn(_ sender: Any) {
+        self.view.backgroundColor = .white
+        self.BottomViewConstraint.constant = self.view.frame.height
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [], animations: {self.view.layoutIfNeeded()})
+    }
+    
+    @IBAction func FirstOptionClick(_ sender: Any) {
+        
+        isFirstOptionViewAppear = !isFirstOptionViewAppear
+        
+        if isFirstOptionViewAppear {
+            
+            self.FirstOptionConstraint.constant = 260
+            self.SecondOptionConstraint.constant = 0
+            self.stackViewConnected.constant = 63.5 + 260
+            self.FirstOptionView.isHidden = false
+            self.SecondOptionView.isHidden = true
+            
+        } else {
+            
+            self.FirstOptionConstraint.constant = 0
+            self.SecondOptionConstraint.constant = 0
+            self.stackViewConnected.constant = 63.5 
+            self.FirstOptionView.isHidden = true
+            self.SecondOptionView.isHidden = true
+            
+        }
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [], animations: {self.view.layoutIfNeeded()})
+        
+    }
+    
+    @IBAction func SecondOptionClick(_ sender: Any) {
+        
+        isSecondOptionViewAppear = !isSecondOptionViewAppear
+        
+        if isSecondOptionViewAppear {
+            
+            self.SecondOptionConstraint.constant = 260
+            self.FirstOptionConstraint.constant = 0
+            self.FirstOptionView.isHidden = true
+            self.SecondOptionView.isHidden = false
+        } else {
+            self.SecondOptionConstraint.constant = 0
+            self.FirstOptionConstraint.constant = 0
+           self.FirstOptionView.isHidden = true
+           self.SecondOptionView.isHidden = true
+        }
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [], animations: {self.view.layoutIfNeeded()})
+        
+        
     }
     
     
