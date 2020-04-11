aab7725662f9d925abd1e85bca9007de66f15ebd
jmzzang <dream7739@naver.com>
2020-04-11 17:46:56 +0900

    로그인 진입화면, 로그인 메인화면 UI 코드 작성

--- 90's/90's/SignIn/LoginMainViewController.swift
+++ 90's/90's/SignIn/LoginMainViewController.swift
@@ -8,23 +8,114 @@
 
 import UIKit
 
-class LoginMainViewController: UIViewController {
-
+class LoginMainViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var emailValidationLabel: UILabel!
+    @IBOutlet weak var selectorImageView1: UIImageView!
+    @IBOutlet weak var tfPass: UITextField!
+    @IBOutlet weak var passValidationLabel: UILabel!
+    @IBOutlet weak var selectorImageView2: UIImageView!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    let signSB = UIStoryboard.init(name: "SignUp", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tfPass.delegate = self
+        tfEmail.delegate = self
+        setUI()
+        setTextFieldObserver()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
+    
+    @IBAction func goLogin(_ sender: Any) {
+        //이메일 형식 검사 후 로그인 형식 맞지 않을 시 메시지 표시
+        
+        let email = tfEmail.text!
+        if(!email.validateEmail()){
+            emailValidationLabel.isHidden = false
+            selectorImageView1.backgroundColor = UIColor.red
+        }else{
+            //로그인 통신
+            //로그인 통신 후 로그인 실패시 메시지 표시
+        }
+        
+    }
+    
+    @IBAction func goFindPass(_ sender: Any) {
+        let authenVC =  signSB.instantiateViewController(withIdentifier: "TelephoneAuthenViewController") as!
+        TelephoneAuthenViewController
+        self.navigationController?.pushViewController(authenVC, animated: true)
+        
+    }
+    
+    @IBAction func goFindEmail(_ sender: Any) {
+        let authenVC =  signSB.instantiateViewController(withIdentifier: "TelephoneAuthenViewController") as!
+        TelephoneAuthenViewController
+        self.navigationController?.pushViewController(authenVC, animated: true)
+    }
+    
+    //화면 터치시 키보드 내림
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tfEmail.endEditing(true)
+        tfPass.endEditing(true)
+    }
+    
+    //키보드 리턴 버튼 클릭 시 키보드 내림
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if(textField == tfEmail){
+            tfPass.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    func setUI(){
+        emailValidationLabel.isHidden = true
+        passValidationLabel.isHidden = true
+        tfEmail.becomeFirstResponder()
+        tfPass.isEnabled = false
+        loginBtn.isEnabled = false
+    }
 
+    //TextField에 대한 옵저버 처리
+    func setTextFieldObserver(){
+        //새로운 패스워드 TF에 대한 옵저버
+        NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: tfEmail, queue: .main, using : {
+            _ in
+            let str = self.tfEmail.text!.trimmingCharacters(in: .whitespaces)
+            
+            if(str != ""){
+                self.selectorImageView1.backgroundColor = UIColor.black
+                self.tfPass.isEnabled = true
+            }else {
+                self.selectorImageView1.backgroundColor = UIColor.gray
+                self.tfPass.isEnabled = false
+            }
+            
+        })
+        
+        
+        //패스워드 확인 TF에 대한 옵저버
+        NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: tfPass, queue: .main, using : {
+            _ in
+            let str = self.tfPass.text!.trimmingCharacters(in: .whitespaces)
+            
+            if(str != ""){
+                self.selectorImageView2.backgroundColor = UIColor.black
+                self.loginBtn.backgroundColor = UIColor.black
+                self.loginBtn.isEnabled = true
+            }else {
+                self.selectorImageView2.backgroundColor = UIColor.gray
+                self.loginBtn.backgroundColor = UIColor.gray
+                self.loginBtn.isEnabled = false
+            }
+            
+        })
+    }
+    
+    
 }
