f30eb50e6b1d5f4c2585ab59efe78d05f24515f9
jmzzang <dream7739@naver.com>
2020-04-11 16:56:44 +0900

    로그인 스토리보드 UI 구성

---
+++ 90's/90's/SignIn/EnterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EnterViewController.swift
+//  90's
+//
+//  Created by 홍정민 on 2020/04/11.
+//  Copyright © 2020 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //둘러보기 버튼 클릭 시
+    @IBAction func takeALook(_ sender: Any) {
+    }
+    
+    //로그인 버튼 클릭 시
+    @IBAction func goSignIn(_ sender: Any) {
+    }
+    
+    //회원가입 버튼 클릭 시
+    @IBAction func goSignUp(_ sender: Any) {
+    }
+    
+
+}
