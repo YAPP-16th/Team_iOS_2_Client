5f2b53754872a2e3d31a6914516ec92ca70a4b7d
jmzzang <dream7739@naver.com>
2020-04-18 03:53:56 +0900

    이메일 입력화면 오토레이아웃

--- 90's/90's/SignUp/EmailViewController.swift
+++ 90's/90's/SignUp/EmailViewController.swift
@@ -13,6 +13,7 @@ class EmailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var selectorImageView: UIImageView!
     @IBOutlet weak var emailValidationLabel: UILabel!
     @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var buttonConst: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,31 +36,53 @@ class EmailViewController: UIViewController, UITextFieldDelegate {
             navigationController?.pushViewController(passwordVC, animated: true)
         }else {
             emailValidationLabel.isHidden = false
+            selectorImageView.image = UIImage(named: "path378Red")
         }
     }
     
     func setUI(){
         tfEmail.delegate = self
-        tfEmail.becomeFirstResponder()
         emailValidationLabel.isHidden = true
+        nextBtn.layer.cornerRadius = 8.0
     }
     
     func setObserver(){
+        //tf에 대한 Observer
         NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: tfEmail, queue: .main, using : {
             _ in
             let str = self.tfEmail.text!.trimmingCharacters(in: .whitespaces)
             
             if(str != ""){
-                self.selectorImageView.backgroundColor = UIColor.black
-                self.nextBtn.backgroundColor = UIColor.black
+                self.selectorImageView.image = UIImage(named: "path378Black")
+                self.nextBtn.backgroundColor = UIColor(displayP3Red: 227/255, green: 62/255, blue: 40/255, alpha: 1.0)
                 self.nextBtn.isEnabled = true
             }else {
-                self.selectorImageView.backgroundColor = UIColor.gray
-                self.nextBtn.backgroundColor = UIColor.gray
+                self.selectorImageView.image = UIImage(named: "path378Grey1")
+                self.nextBtn.backgroundColor = UIColor(displayP3Red: 199/255,green: 201/255, blue: 208/255, alpha: 1.0)
                 self.nextBtn.isEnabled = false
             }
-            
         })
+        
+        //키보드에 대한 Observer
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        let keyboardHeight = keyboardSize.cgRectValue.height
+        
+        if(keyboardHeight > 300){
+            buttonConst.constant = keyboardHeight - 18
+        }else{
+            buttonConst.constant = keyboardHeight + 18
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        buttonConst.constant = 18
     }
     
     //화면 터치시 키보드 내림
