3d7481a7de436c5ca0fba9f60ae6a188a42e78c4
jmzzang <dream7739@naver.com>
2020-04-18 21:34:51 +0900

    회원가입 성공 UI 오토레이아웃

--- 90's/90's/SignUp/CompleteViewController.swift
+++ 90's/90's/SignUp/CompleteViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class CompleteViewController: UIViewController {
-
+    @IBOutlet weak var loginBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginBtn.layer.cornerRadius = 8.0
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func goBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
     @IBAction func goLoginVC(_ sender: Any) {
        //로그인 화면으로 가는 로직 구현
     }
