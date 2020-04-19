debf46610a1c414c331b1656196cf6e5482d1154
artificIn <djgmd1021@gmail.com>
2020-04-19 21:40:42 +0900

    complete album layout vc

--- 90's/90's/Album/CreateAlbum/AlbumLayout/AlbumLayoutPreviewVC.swift
+++ 90's/90's/Album/CreateAlbum/AlbumLayout/AlbumLayoutPreviewVC.swift
@@ -49,34 +49,50 @@ extension AlbumLayoutPreviewVC : UICollectionViewDelegate, UICollectionViewDataS
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumlayoutpreviewcell", for: indexPath) as! AlbumLayoutPreviewCell
+        var size : CGSize = CGSize(width: 0, height: 0)
         
         switch selectedLayout {
         case .Polaroid :
             cell.backimageView.image = AlbumLayout.Polaroid.image
             cell.backimageView.frame.size = AlbumLayout.Polaroid.size
-            cell.imageView.frame = CGRect(x: 10, y: 10, width: cell.contentView.frame.size.width - 20, height: cell.contentView.frame.size.height - 40)
+            size = CGSize(width: AlbumLayout.Polaroid.size.width - 20, height: AlbumLayout.Polaroid.size.height - 50)
+            cell.imageView.frame = CGRect(x: 10, y: 10, width: size.width, height: size.height)
         case .Mini :
             cell.backimageView.image = AlbumLayout.Mini.image
             cell.backimageView.frame.size = AlbumLayout.Mini.size
+            size = CGSize(width: AlbumLayout.Mini.size.width - 24, height: AlbumLayout.Mini.size.height - 48)
+            cell.imageView.frame = CGRect(x: 12, y: 9, width: size.width, height: size.height)
         case .Memory :
             cell.backimageView.image = AlbumLayout.Memory.image
             cell.backimageView.frame.size = AlbumLayout.Memory.size
+            size = CGSize(width: AlbumLayout.Memory.size.width - 48, height: AlbumLayout.Memory.size.height - 52)
+            cell.imageView.frame = CGRect(x: 24, y: 26, width: size.width, height: size.height)
         case .Portrab :
             cell.backimageView.image = AlbumLayout.Portrab.image
             cell.backimageView.frame.size = AlbumLayout.Portrab.size
+            size = CGSize(width: AlbumLayout.Portrab.size.width - 20, height: AlbumLayout.Portrab.size.height - 24)
+            cell.imageView.frame = CGRect(x: 10, y: 12, width: size.width, height: size.height)
         case .Tape :
             cell.backimageView.image = AlbumLayout.Tape.image
             cell.backimageView.frame.size = AlbumLayout.Tape.size
+            size = CGSize(width: AlbumLayout.Tape.size.width - 44, height: AlbumLayout.Tape.size.height - 80)
+            cell.imageView.frame = CGRect(x: 23, y: 43, width: size.width, height: size.height)
         case .Portraw :
             cell.backimageView.image = AlbumLayout.Portraw.image
             cell.backimageView.frame.size = AlbumLayout.Portraw.size
+            size = CGSize(width: AlbumLayout.Portraw.size.width - 18, height: AlbumLayout.Portraw.size.height - 30)
+            cell.imageView.frame = CGRect(x: 9, y: 15, width: size.width, height: size.height)
         case .Filmroll :
+            // 이미지 없어서 건너뜀
             cell.backimageView.image = AlbumLayout.Filmroll.image
             cell.backimageView.frame.size = AlbumLayout.Filmroll.size
+            cell.imageView.frame = CGRect(x: 20, y: 2, width: AlbumLayout.Filmroll.size.width - 40, height: AlbumLayout.Filmroll.size.height - 4)
         default: cell.backimageView.image = AlbumLayout.Polaroid.image
         }
-        cell.imageView.image = photoArray[indexPath.row]
-        print("backview size = \(cell.backimageView.frame), image size = \(cell.imageView.frame)")
+        
+        cell.imageView.image = photoArray[indexPath.row].imageResize(sizeChange: size)
+        print("contentview size = \(cell.contentView.frame)")
+        print("imageview size = \(cell.imageView.frame) image size = \(cell.imageView.image?.size)")
 
         return cell
     }
