6bcd082fb8aa5477a769db3cde4163df2955f7fa
jmzzang <dream7739@naver.com>
2020-04-25 20:08:25 +0900

    이메일 찾기 완료 UI 코드 작성 완료

--- 90's/90's/SignIn/FindEmailViewController.swift
+++ 90's/90's/SignIn/FindEmailViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class FindEmailViewController: UIViewController {
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var nickNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUI()
+        //통신을 통해 찾아온 email, nickName 받아옴
     }
     
     @IBAction func goBack(_ sender: Any) {
@@ -21,6 +25,10 @@ class FindEmailViewController: UIViewController {
     
     @IBAction func goLogin(_ sender: Any) {
         navigationController?.popToViewController(self.navigationController!.viewControllers[1], animated: true)
+    }
+    
+    func setUI(){
+        loginBtn.layer.cornerRadius = 8.0
     }
     
 }
