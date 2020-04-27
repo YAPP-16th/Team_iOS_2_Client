d5a720b573436424cd147f3be44167a572f41621
artificIn <djgmd1021@gmail.com>
2020-04-27 19:19:15 +0900

    complete Sticker resizing

--- 90's/90's/Sticker/StickerLayout.swift
+++ 90's/90's/Sticker/StickerLayout.swift
@@ -10,36 +10,26 @@ import UIKit
 
 
 class StickerLayout: UIView {
-    @IBOutlet weak var stickerView: UIView!
     @IBOutlet weak var stickerImageView: UIImageView!
     @IBOutlet weak var backImageView: UIImageView!
-    @IBOutlet weak var rotateBtn: UIButton!
-    @IBOutlet weak var resizeBtn: UIButton!
+    @IBOutlet weak var rotateImageView: UIImageView!
+    @IBOutlet weak var resizeImageView: UIImageView!
     @IBAction func cancleBtn(_ sender: UIButton) {
         self.removeFromSuperview()
     }
     
-    @IBAction func sizeBtn(_ sender: UIButton) {
-    }
-    @IBAction func turnBtn(_ sender: UIButton) {
-    }
-
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setting()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setting()
     }
     
-    func setting(){
-        backImageView.image = UIImage(named: "stickerControlBox")!
-        rotateBtn.imageView?.image = UIImage(named: "stickerControlRotate")!
-        resizeBtn.imageView?.image = UIImage(named: "stickerControlSize")!
-        self.isUserInteractionEnabled = true
+    class func loadFromZib(image : UIImage) -> StickerLayout {
+        let view = Bundle.main.loadNibNamed("StickerLayout", owner: self, options: nil)?.first as! StickerLayout
+        view.stickerImageView.image = image
+        return view
     }
 }
 
--- 90's/90's/Sticker/panVC.swift
+++ 90's/90's/Sticker/panVC.swift
@@ -14,24 +14,31 @@ class panVC: UIViewController {
     var sticker : StickerLayout!
     var testimage : UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         createStickerView(image: UIImage(named: "smileimage")!)
-//        sticker.imageView.image = UIImage(named: "smileimage")!
         createView(image: UIImage(named: "starimage")!)
     }
-    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         
-        if touch!.view == sticker {
-            print("rotate moved detected")
+        if touch!.view == sticker.rotateImageView {
             let position = touch!.location(in: self.view)
             let target = sticker?.center
             let angle = atan2(target!.y - position.y, target!.x - position.x)
-            sticker?.transform = CGAffineTransform(rotationAngle: angle)
+//            sticker?.transform = sticker.transform.rotated(by: angle)
+//            target = position
+//            sticker?.transform = CGAffineTransform(scaleX: target!.x / position.x, y: target!.y / position.y)
+            
+//            sticker?.transform = CGAffineTransform(rotationAngle: angle)
+        }
+        else if touch!.view == sticker.resizeImageView {
+            let position = touch!.location(in: self.view)
+            let target = sticker?.center
+            let angle = atan2(target!.y - position.y, target!.x - position.x)
+            let size = max((position.x / target!.x), (position.y / target!.y))
+            sticker?.transform = CGAffineTransform(scaleX: size, y: size)
         }
         else if touch!.view == testView {
             let position = touch!.location(in: self.view)
@@ -56,56 +63,42 @@ extension panVC {
         imageView.isUserInteractionEnabled = true
         imageView.image = image
         testimage = imageView
-        createImageRotate(view: imageView)
-        self.view.addSubview(imageView)
+        //createImageRotate(view: imageView)
+        self.view.addSubview(testimage)
     }
     
     private func createStickerView(image : UIImage){
-        let imageView : StickerLayout = StickerLayout(frame: CGRect(x: self.view.frame.width / 3 - 50, y: self.view.frame.height / 6 - 50, width: 120, height: 120))
-        imageView.awakeFromNib()
-        //imageView.stickerImageView.image = image
-//        let imageView = Bundle.main.loadNibNamed("StickerLayout", owner: self, options: nil)?.first as! StickerLayout
-//        imageView.frame = CGRect(x: self.view.frame.width / 3 - 50, y: self.view.frame.height / 6 - 50, width: 120, height: 120)
-//        imageView.imageView.image = image
-//        imageView.isUserInteractionEnabled = true
-        
+        let imageView = StickerLayout.loadFromZib(image: image)
+        imageView.frame = CGRect(x: self.view.frame.width / 2 - 60, y: self.view.frame.height / 2 - 60, width: 120, height: 120)
         sticker = imageView
-        createPan(view: imageView)
-        createRotate(view: imageView)
-        self.view.addSubview(imageView)
+//        createPan(view: sticker)
+        createRotate(view: sticker.rotateImageView)
+    
+        self.view.addSubview(sticker)
     }
     
-    private func createPan(view : StickerLayout){
+    private func createPan(view : UIView){
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(panGesture:)))
         view.addGestureRecognizer(panGesture)
     }
     
-    private func createRotate(view : StickerLayout){
-        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotateGesture(rotateGesture:)))
+    private func createRotate(view : UIImageView){
+        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(self.handleRotateGesture(rotateGesture:)))
         view.addGestureRecognizer(rotateGesture)
     }
     
-    private func createImageRotate(view : UIImageView){
-        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotateGesture(rotateGesture:)))
-        view.addGestureRecognizer(rotateGesture)
-    }
-    
-    
-    @objc func handleRotateGesture(rotateGesture: UIRotationGestureRecognizer){
-        let radians = rotateGesture.rotation
-        let velocity = rotateGesture.velocity
-        let result = "Rotation - Randians = \(radians), velocity = \(velocity)"
-        print("result = \(result)")
-    }
     
     @objc func handlePanGesture(panGesture: UIPanGestureRecognizer){
         let transition = panGesture.translation(in: sticker)
         panGesture.setTranslation(CGPoint.zero, in: sticker)
+        sticker.center = CGPoint(x: sticker.center.x + transition.x, y: sticker.center.y + transition.y)
+    }
+    
+    @objc func handleRotateGesture(rotateGesture : UIRotationGestureRecognizer){
+        sticker!.layer.anchorPoint = CGPoint(x: sticker!.center.x, y: sticker!.center.y)
+        sticker!.transform = sticker!.transform.rotated(by: rotateGesture.rotation)
         
-        let imageView = panGesture.view as! StickerLayout
-        imageView.center = CGPoint(x: imageView.center.x + transition.x, y: imageView.center.y + transition.y)
-        imageView.isUserInteractionEnabled = true
-        imageView.isMultipleTouchEnabled = false
-        self.view.addSubview(imageView)
+        rotateGesture.rotation = 0
+        print("rotate gesture working")
     }
 }
