171e56f8fe428d98cc22cfecfc38b998a79acb93
jmzzang <dream7739@naver.com>
2020-05-02 18:44:53 +0900

    내정보 관리 메뉴 메인 UI 코드 작성

--- 90's/90's/Profile/ManageInfoViewController.swift
+++ 90's/90's/Profile/ManageInfoViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class ManageInfoViewController: UIViewController {
 
+    @IBOutlet weak var infoTableView: UITableView!
+    var infoList = ["이메일 변경", "비밀번호 변경", "전화번호 변경"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI(){
+        infoTableView.delegate = self
+        infoTableView.dataSource = self
     }
-    */
 
+}
+
+extension ManageInfoViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return infoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell") as! InfoCell
+        cell.infoNameLabel.text = infoList[indexPath.row]
+        return cell
+    }
+    
+    
 }
