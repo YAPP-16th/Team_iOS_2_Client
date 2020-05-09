bf4e8c913520e72e462aed6072af75b0e9c446fd
artificIn <djgmd1021@gmail.com>
2020-05-09 15:17:28 +0900

    Carthage add KingFisher

--- 90's/90's/Extension/Extension.swift
+++ 90's/90's/Extension/Extension.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import Kingfisher
 
 
 extension String {
@@ -405,6 +406,22 @@ extension UIView {
         } else {
             // borderWidth 변수가 nil 일 경우의 default
             self.layer.borderWidth = 1.0
+        }
+    }
+}
+
+
+extension UIImageView {
+    public func imageFromUrl(_ urlString: String?, defaultImgPath : String) {
+        let defaultImg = UIImage(named: defaultImgPath)
+        if let url = urlString {
+            if url.isEmpty {
+                self.image = defaultImg
+            } else {
+                self.kf.setImage(with: URL(string: url), placeholder: defaultImg, options: [.transition(ImageTransition.fade(0.5))])
+            }
+        } else {
+            self.image = defaultImg
         }
     }
 }
